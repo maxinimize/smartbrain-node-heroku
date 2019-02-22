73a474a8b28a3fa2a487a23b8a46e0987a69d56b
maxinimize <maxinimize@gmail.com>
2019-02-22 18:21:39 -0500

    heroku database

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host : '127.0.0.1',
-		user : 's73v3n',
-		password : '',
-		database : 'smart-brain'
+    // heroku database
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+		// host : '127.0.0.1',
+		// user : 's73v3n',
+		// password : '',
+		// database : 'smart-brain'
 	}
 });
 
@@ -31,7 +34,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) } )
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) } )
 
-
+// heroku deployment
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`app is on port ${process.env.PORT}`);
 })
